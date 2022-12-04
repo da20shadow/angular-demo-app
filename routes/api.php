ea4c93bd67a973276dbbc3f740f86642239d5670
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-04 19:06:14 +0200

    Added Routes for user
    Removed the laravel comment and
    Added all routes for the user api requests.

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,22 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+/** --------------Public routes-------------- */
+Route::post('register',[UserController::class,'register']);
+Route::post('login',[UserController::class,'login']);
+Route::post('user/{id}',[UserController::class,'show']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/** --------------Protected routes-------------- */
+
+    /** -----User routes----- */
+    Route::get('profile',[UserController::class,'index']);
+    Route::patch('profile/update',[UserController::class,'update']);
+    Route::delete('profile/delete',[UserController::class,'destroy']);
+    Route::post('logout',[UserController::class,'logout']);
+
+    /** -----GOALS routes----- */
+    //TODO: add goals routes
+    /** -----TASKS routes----- */
+    //TODO: add tasks routes
