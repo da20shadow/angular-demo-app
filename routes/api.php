e9f493217719b830aa644c582abd86be524a32ce
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-04 20:41:08 +0200

    Added middleware to protect the routes
    Added goals routes and surrounded in middleware to protect them all.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GoalController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,14 +10,20 @@ Route::post('login',[UserController::class,'login']);
 Route::get('user/{id}',[UserController::class,'show']);
 
 /** --------------Protected routes-------------- */
-
+Route::group(['middleware' => ['auth:sanctum']],function () {
     /** -----User routes----- */
-    Route::get('profile',[UserController::class,'index']);
-    Route::patch('profile/update',[UserController::class,'update']);
-    Route::delete('profile/delete',[UserController::class,'destroy']);
-    Route::post('logout',[UserController::class,'logout']);
+    Route::get('profile', [UserController::class, 'index']);
+    Route::patch('profile/update', [UserController::class, 'update']);
+    Route::delete('profile/delete', [UserController::class, 'destroy']);
+    Route::post('logout', [UserController::class, 'logout']);
 
     /** -----GOALS routes----- */
-    //TODO: add goals routes
+    Route::get('goals', [GoalController::class, 'index']);
+    Route::get('goals/{id}', [GoalController::class, 'show']);
+    Route::post('goals/add', [GoalController::class, 'store']);
+    Route::patch('goals/update/{id}', [GoalController::class, 'update']);
+    Route::delete('goals/delete/{id}', [GoalController::class, 'destroy']);
+
     /** -----TASKS routes----- */
     //TODO: add tasks routes
+});
