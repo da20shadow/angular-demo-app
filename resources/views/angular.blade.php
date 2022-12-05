2ff45fcead33f2285d5b6b6cab9b8df4e44a1465
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-05 12:07:00 +0200

    Angular blade file

---
+++ resources/views/angular.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Angular</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" href="styles.ef46db3751d8e999.css">
+</head>
+<body>
+<app-root></app-root>
+<script src="/assets/angular/runtime.27845ceb5aedcb1f.js" type="module"></script>
+<script src="/assets/angular/polyfills.55e452bef9dff314.js" type="module"></script>
+<script src="/assets/angular/main.c203bbcdad06c707.js" type="module"></script>
+</body>
+</html>
