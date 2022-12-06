a2e78049c7b81555df8f81e677f4347defd4e055
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-07 00:18:07 +0200

    Added paths in the Routes

--- resources/angular/src/app/app-routing.module.ts
+++ resources/angular/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./public_pages/home/home.component";
+import {FaqComponent} from "./public_pages/faq/faq.component";
+import {NotFoundComponent} from "./public_pages/not-found/not-found.component";
+import {NoAccessComponent} from "./public_pages/no-access/no-access.component";
+import {LoginComponent} from "./auth/login/login.component";
+import {RegisterComponent} from "./auth/register/register.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'faq', component: FaqComponent},
+  {path: 'no-access', component: NoAccessComponent},
+  {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
