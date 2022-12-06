d36659271a8a099a7c6e2dca9c8f6540f72a41ae
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-07 00:08:20 +0200

    Created Not Found and No access components

--- resources/angular/src/app/public_pages/public-pages.module.ts
+++ resources/angular/src/app/public_pages/public-pages.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NoAccessComponent } from './no-access/no-access.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
+    NotFoundComponent,
+    NoAccessComponent,
   ],
   imports: [
     CommonModule
   ],
-  exports: [HomeComponent]
+  exports: [HomeComponent,NotFoundComponent,NoAccessComponent]
 })
 export class PublicPagesModule { }
