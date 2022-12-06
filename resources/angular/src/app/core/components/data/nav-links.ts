4e0e5e0a36b1cde601fdea090046c70b97921c79
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-07 00:18:28 +0200

    Created public and private nav-links objects

---
+++ resources/angular/src/app/core/components/data/nav-links.ts
@@ -0,0 +1,19 @@
+export const publicNavLinks = [
+  {url: '/', name: 'Home'},
+  {url: 'faq', name: 'FAQ'},
+  {url: 'contact-us', name: 'Contact us'},
+  {url: 'login', name: 'Login'},
+  {url: 'register', name: 'Register'},
+];
+
+export const privateNavLinks = [
+  {url: 'dashboard', name: 'Dashboard'},
+  {url: 'profile', name: 'Profile'},
+  {url: 'goals', name: 'Goals'},
+  {url: 'agenda', name: 'Agenda'},
+  {url: 'logout', name: 'Logout'},
+];
+
+export const footerNavLinks = [
+  //TODO:
+];
