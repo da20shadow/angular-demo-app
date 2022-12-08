8978cab5fb71757e38e13219a24ea779540ad5d7
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-08 12:17:46 +0200

    Created task model, status and priority constants

---
+++ resources/angular/src/app/shared/constants/Status.ts
@@ -0,0 +1,6 @@
+export class Status {
+  static readonly TO_DO = 'To Do';
+  static readonly IN_PROGRESS = 'In Progress';
+  static readonly IN_REVIEW = 'In Revision';
+  static readonly COMPLETED = 'Completed';
+}
