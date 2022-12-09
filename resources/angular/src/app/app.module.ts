2ffc3632dd71e14ee2c2492fe42e3cce9e27305a
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-09 22:47:18 +0200

    Imported interceptor settings and JWT interceptor

--- resources/angular/src/app/app.module.ts
+++ resources/angular/src/app/app.module.ts
@@ -1,30 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {SharedModule} from "./shared/shared.module";
+import { AppRoutingModule } from './app-routing.module';
+
 import {CoreModule} from "./core/core.module";
+import {SharedModule} from "./shared/shared.module";
 import {PublicPagesModule} from "./public_pages/public-pages.module";
-import {AuthModule} from "./auth/auth.module";
+
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import {AuthModule} from "./auth/auth.module";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {JWTInterceptorService} from "./auth/interceptors/jwt-interceptor";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserModule,
+    HttpClientModule,
     CoreModule,
     SharedModule,
     PublicPagesModule,
     AuthModule,
-    BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([])
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JWTInterceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
