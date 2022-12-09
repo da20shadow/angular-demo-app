c65080a809ba54bfa6c1b85e81a4464d2b57614c
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-09 22:52:36 +0200

    Implemented header methods
    For login checking navbar changing etc

--- resources/angular/src/app/core/components/header/header.component.ts
+++ resources/angular/src/app/core/components/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {navBar} from "../../data/nav-links";
+import {DarkLightModeService} from "../../services/dark-light-mode.service";
+import {AuthService} from "../../../auth/services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -7,4 +11,42 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  isLogged!: boolean;
+  darkMode!: boolean;
+  navbar: any = [];
+
+  constructor(private darkModeService: DarkLightModeService,
+              private route: Router,
+              private auth: AuthService) {
+    this.isLogged = this.auth.isLoggedIn();
+    this.changeNavigation();
+    this.darkMode = this.darkModeService.checkUserPreferredMode();
+  }
+
+  ngOnInit() {
+    this.route.events.subscribe(event => {
+      if (event.constructor.name === "NavigationEnd") {
+        this.isLogged = this.auth.isLoggedIn();
+        this.changeNavigation();
+      }
+    })
+  }
+
+  changeNavigation(){
+    if (this.isLogged){
+      this.navbar = navBar.privateNavLinks;
+    }else {
+      this.navbar = navBar.publicNavLinks;
+    }
+  }
+
+  toggleDarkMode() {
+    if (this.darkMode){
+      this.darkModeService.enableLightTheme()
+      this.darkMode = false;
+    }else{
+      this.darkModeService.enableDarkTheme()
+      this.darkMode = true;
+    }
+  }
 }
