3b81651c9dbbcfba610e21bab784522bee63bf35
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-09 22:53:09 +0200

    Created User interface

---
+++ resources/angular/src/app/core/models/User.ts
@@ -0,0 +1,9 @@
+export interface User {
+  id: number|undefined|null;
+  email: string|undefined|null;
+  username: string|undefined|null;
+  created_at: string|undefined|null;
+  email_verified_at: string|undefined|null;
+  updated_at: string|undefined|null;
+  token: string|undefined|null;
+}
