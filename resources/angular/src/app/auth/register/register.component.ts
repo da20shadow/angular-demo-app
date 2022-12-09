eb9d7ffb22673ea089997d7cfebe0560075a811f
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-09 22:50:03 +0200

    registration implemented

--- resources/angular/src/app/auth/register/register.component.ts
+++ resources/angular/src/app/auth/register/register.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import {Title} from "@angular/platform-browser";
+import {GlobalClasses} from "../../shared/constants/Global-Classes";
+import {FormBuilder, NgForm, Validators} from "@angular/forms";
+import {AuthService} from "../services/auth.service";
+import {emailValidator} from "../../shared/validators/email.validator";
+import {sameValueValidator} from "../../shared/validators/same-value.validator";
+import {DarkLightModeService} from "../../core/services/dark-light-mode.service";
 
 @Component({
   selector: 'app-register',
@@ -8,8 +14,44 @@ import {Title} from "@angular/platform-browser";
 })
 export class RegisterComponent {
 
-  constructor(private titleService: Title) {
+  globalClasses = GlobalClasses;
+  isDarkMode: boolean = this.darkModeService.isDarkModeEnabled();
+
+  regForm = this.fb.group({
+    username: ['', [Validators.required,Validators.minLength(3)]],
+    email: ['', [Validators.required, emailValidator()]],
+    pass: this.fb.group({
+
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      rePassword: ['', [Validators.required, Validators.minLength(8)]],
+
+    },{validators: [sameValueValidator("password","rePassword")]})
+  })
+
+  constructor(private titleService: Title,
+              private fb: FormBuilder,
+              private darkModeService: DarkLightModeService,
+              private auth: AuthService) {
     this.titleService.setTitle('Register - GoalsApp')
   }
 
+  registerHandler() {
+    //TODO: show notification instead alert!
+    if (this.regForm.invalid) {
+      alert("Invalid Form Fields!");
+      return;
+    }
+    this.auth.register(this.regForm.value).subscribe({
+      next: value => {
+        console.log('Success: ',value)
+        alert(value)
+      },
+      error: (err) => {
+        console.log('err' + err)
+        alert('Error: ' + err.error.message)
+        console.log('Error: ',err.error.message)
+      }
+    });
+
+  }
 }
