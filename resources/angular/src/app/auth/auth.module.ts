13de27de5eb8e3059cf6f30781f93b31364fc6bd
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-09 22:47:38 +0200

    Imported needed modules in auth module

--- resources/angular/src/app/auth/auth.module.ts
+++ resources/angular/src/app/auth/auth.module.ts
@@ -2,16 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
-
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {SharedModule} from "../shared/shared.module";
+import { LogoutComponent } from './logout/logout.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
     RegisterComponent,
+    LogoutComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    SharedModule,
+
   ],
   exports: [LoginComponent,RegisterComponent]
 })
