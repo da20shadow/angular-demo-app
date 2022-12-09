faae3698ade9443529029f7fd069b5b5ec96ef3b
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-09 22:50:28 +0200

    Auth service all methods implemented

--- resources/angular/src/app/auth/services/auth.service.ts
+++ resources/angular/src/app/auth/services/auth.service.ts
@@ -1,15 +1,52 @@
 import { Injectable } from '@angular/core';
-import {of} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {ApiUrls} from "../../shared/constants/api-urls";
+import {User} from "../../core/models/User";
+import {Router} from "@angular/router";
+import {tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string){
+    return this.http.post<User>(ApiUrls.LOGIN, {email, password});
+  }
+
+  logout(){
+    return this.http.post(ApiUrls.LOGOUT, {}).pipe(
+      tap(r =>
+        this.removeToken()
+      )
+    );
+  }
 
   isLoggedIn(){
-    return of(false);
-    //TODO:
+    return !!this.getToken();
+  }
+
+  getToken() {
+    const token = localStorage.getItem('token');
+    return token ? token : undefined;
+  }
+
+  removeToken() {
+    localStorage.removeItem('token');
+  }
+
+  register(regForm: any) {
+
+    const {username,email,pass} = regForm;
+    const {password,rePassword} = pass;
+
+    return this.http.post(ApiUrls.REGISTER,{
+      username,
+      email,
+      password,
+      password_confirmation: rePassword
+    });
   }
 }
