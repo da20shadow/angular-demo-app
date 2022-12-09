4233ea3c7c604cffd4507a2a0051198555551461
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-09 22:55:25 +0200

    Updated islogged in guard

--- resources/angular/src/app/shared/guards/isLoggedInGuardFn.ts
+++ resources/angular/src/app/shared/guards/isLoggedInGuardFn.ts
@@ -6,7 +6,8 @@ export const isLoggedInGuardFn: CanActivateFn = () => {
   const router = inject(Router);
   const isLogged = inject(AuthService).isLoggedIn();
   if (!isLogged){
-    router.navigate(['no-access']);
+    router.navigate(['no-access']).then(r => {
+      console.log(r)})
   }
   return isLogged;
 }
