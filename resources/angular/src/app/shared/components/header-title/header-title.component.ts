965e8ba64ba26865095310a3c9a539eb7b77b41b
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-09 22:54:59 +0200

    Created Page Header Title component

---
+++ resources/angular/src/app/shared/components/header-title/header-title.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+import {GlobalClasses} from "../../constants/Global-Classes";
+
+@Component({
+  selector: 'app-header-title',
+  templateUrl: './header-title.component.html',
+  styleUrls: ['./header-title.component.scss']
+})
+export class HeaderTitleComponent {
+  @Input() title: string = '';
+  classes = GlobalClasses;
+
+}
