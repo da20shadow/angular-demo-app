40d5f45d6241dd91dee4bf63b545268331ed6e2d
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-10 23:59:10 +0200

    Created enum for goal categories

---
+++ resources/angular/src/app/shared/constants/GoalCategories.ts
@@ -0,0 +1,10 @@
+export enum GoalCategories {
+  HEALTH = 'Health',
+  CAREER = 'Career',
+  PERSONAL = 'Personal',
+  SPIRITUAL = 'Spiritual',
+  FINANCIAL = 'Financial',
+  EDUCATIONAL = 'Educational',
+  RELATIONSHIP = 'Relationship',
+  OTHER = 'Other'
+}
