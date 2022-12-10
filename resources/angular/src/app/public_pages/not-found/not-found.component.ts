16294f5f1dfda5e6ec1385a29625d46c6a529339
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-10 23:58:51 +0200

    imported global classes

--- resources/angular/src/app/public_pages/not-found/not-found.component.ts
+++ resources/angular/src/app/public_pages/not-found/not-found.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Title} from "@angular/platform-browser";
+import {GlobalClasses} from "../../shared/constants/Global-Classes";
 
 @Component({
   selector: 'app-not-found',
@@ -7,6 +8,8 @@ import {Title} from "@angular/platform-browser";
   styleUrls: ['./not-found.component.scss']
 })
 export class NotFoundComponent {
+
+  classes = GlobalClasses;
 
   constructor(private titleService: Title) {
     this.titleService.setTitle('404 Not Found - GoalsApp')
