ad3e60e97fce0d939ea09d9d6d84dec3ca0aa3ee
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-12 21:04:38 +0200

    Imported components in public pages module

--- resources/angular/src/app/public_pages/public-pages.module.ts
+++ resources/angular/src/app/public_pages/public-pages.module.ts
@@ -4,6 +4,9 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NoAccessComponent } from './no-access/no-access.component';
 import { FaqComponent } from './faq/faq.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import {RouterModule} from "@angular/router";
+import {SharedModule} from "../shared/shared.module";
 
 
 @NgModule({
@@ -12,15 +15,19 @@ import { FaqComponent } from './faq/faq.component';
     NotFoundComponent,
     NoAccessComponent,
     FaqComponent,
+    ContactUsComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    SharedModule,
   ],
   exports: [
     HomeComponent,
     NotFoundComponent,
     NoAccessComponent,
-    FaqComponent
+    FaqComponent,
+    ContactUsComponent
   ]
 })
 export class PublicPagesModule { }
