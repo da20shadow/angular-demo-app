05c836889713fb6375b3a77cda30122116b88bf6
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-14 23:34:04 +0200

    added declarations

--- resources/angular/src/app/core/core.module.ts
+++ resources/angular/src/app/core/core.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import {RouterModule} from "@angular/router";
-
+import { PublicNavigationComponent } from './components/header/components/public-navigation/public-navigation.component';
+import { PrivateNavigationComponent } from './components/header/components/private-navigation/private-navigation.component';
+import { NavLinksComponent } from './components/header/components/nav-links/nav-links.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PublicNavigationComponent,
+    PrivateNavigationComponent,
+    NavLinksComponent,
   ],
   imports: [
     CommonModule,
