ee26877d561f42c07bb84dcb7c5c3ac519176617
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-15 23:17:01 +0200

    implemented goal details page

--- resources/angular/src/app/goals/features/goal-details/goal-details.component.ts
+++ resources/angular/src/app/goals/features/goal-details/goal-details.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Store} from "@ngrx/store";
+import {GlobalClasses} from "../../../shared/constants/Global-Classes";
+import {Goal} from "../../../core/models";
+import {Title} from "@angular/platform-browser";
+import {Observable} from "rxjs";
+import {goalsSelectors} from "../../../Store/app.state";
+import {GoalPageActions} from "../../../Store/goals-store/goals-page.actions";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-goal-details',
@@ -6,5 +15,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./goal-details.component.scss']
 })
 export class GoalDetailsComponent {
+
+  classes = GlobalClasses;
+  goal$: Observable<Goal|undefined>;
+  editGoal:boolean = false;
+  constructor(private title: Title,
+              private route: ActivatedRoute,
+              private store$: Store) {
+    //TODO: add the goal title as title of the page!
+    this.title.setTitle('Goal - GoalsApp')
+    this.goal$ = this.store$.select(goalsSelectors.activeGoal);
+    this.store$.dispatch(GoalPageActions.getGoalById())
+  }
+
+  updateTask(taskInfo: any){
+    this.store$.dispatch(GoalPageActions.updateTask({taskId:taskInfo.taskId,task:taskInfo.task}));
+  }
+
+  doSomething() {
+    alert('Title Clicked!')
+    //TODO Remove this!
+    console.log('goal', this.goal$)
+  }
+
+  editGoalFormHandler(editGoalForm: NgForm) {
+    if (editGoalForm.invalid){alert('Invalid Form Fields!'); return;}
+    console.log(editGoalForm.value)
+    const changed = editGoalForm.value;
+    this.store$.dispatch(GoalPageActions.updateGoal({goalId: changed.id, changed }))
+    this.editGoal = false;
+  }
 
 }
--- resources/angular/src/app/goals/features/goal-details/goal-details.module.ts
+++ resources/angular/src/app/goals/features/goal-details/goal-details.module.ts
@@ -1,17 +1,34 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { GoalDetailsRoutingModule } from './goal-details-routing.module';
-import { GoalDetailsComponent } from './goal-details.component';
+import {GoalDetailsRoutingModule} from './goal-details-routing.module';
+import {GoalDetailsComponent} from './goal-details.component';
+import {SharedModule} from "../../../shared/shared.module";
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {FormsModule} from "@angular/forms";
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {GoalTasksComponent} from "./goal-tasks/goal-tasks.component";
+import {MatSortModule} from '@angular/material/sort';
+import {MatDialogModule} from '@angular/material/dialog';
 
 
 @NgModule({
   declarations: [
-    GoalDetailsComponent
+    GoalDetailsComponent,
+    GoalTasksComponent
   ],
   imports: [
     CommonModule,
-    GoalDetailsRoutingModule
-  ]
+    MatMenuModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatSortModule,
+    MatDialogModule,
+    FormsModule,
+    GoalDetailsRoutingModule,
+    SharedModule,
+  ],
 })
-export class GoalDetailsModule { }
+export class GoalDetailsModule {
+}
