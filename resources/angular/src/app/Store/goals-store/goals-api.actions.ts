1f63dd9ad6591144325048860d21ff156674322e
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-12-15 23:24:30 +0200

    added goals api actions

--- resources/angular/src/app/Store/goals-store/goals-api.actions.ts
+++ resources/angular/src/app/Store/goals-store/goals-api.actions.ts
@@ -1,22 +1,35 @@
 import {createAction, props} from "@ngrx/store";
-import {AddGoalApiResult, DeleteGoalApiResult, Goal, UpdateGoalApiResult} from "../../core/models";
+import { Goal, GoalApiResponse,TaskApiResponse} from "../../core/models";
 
 const loadSuccess = createAction('[GOALS API] Load Goals Success', props<{ goals: Goal[] }>());
-const loadFailure = createAction('[GOALS API] Load Goals Failure', props<{ error: string }>());
-const createSuccess = createAction('[GOALS API] Create Goal Success', props<{ response: AddGoalApiResult }>());
-const createFailure = createAction('[GOALS API] Create Goal Failure', props<{ error: string }>());
-const updateSuccess = createAction('[GOALS API] Update Goal Success', props<{ response: UpdateGoalApiResult }>());
-const updateFailure = createAction('[GOALS API] Update Goal Failure', props<{ error: string }>());
-const deleteSuccess = createAction('[GOALS API] Delete Goal Success', props<{ response: DeleteGoalApiResult }>());
-const deleteFailure = createAction('[GOALS API] Delete Goal Failure', props<{ error: string }>());
+const loadFailure = createAction('[GOALS API] Load Goals Failure', props<{ error: GoalApiResponse }>());
+
+const createSuccess = createAction('[GOALS API] Create Goal Success', props<{ response: GoalApiResponse }>());
+const createFailure = createAction('[GOALS API] Create Goal Failure', props<{ error: GoalApiResponse }>());
+
+const updateSuccess = createAction('[GOALS API] Update Goal Success', props<{ response: GoalApiResponse }>());
+const updateFailure = createAction('[GOALS API] Update Goal Failure', props<{ error: GoalApiResponse }>());
+
+const deleteSuccess = createAction('[GOALS API] Delete Goal Success', props<{ response: GoalApiResponse }>());
+const deleteFailure = createAction('[GOALS API] Delete Goal Failure', props<{ error: GoalApiResponse }>());
+
+const getGoalByIdSuccess = createAction('[GOALS API] Get Goal Success', props<{ response: GoalApiResponse }>());
+const getGoalByIdFailure = createAction('[GOALS API] Get Goal Failure', props<{ error: GoalApiResponse }>());
+
+const updateTaskSuccess = createAction('[GOAL API] Update Task Success',props<{response: TaskApiResponse}>());
+const updateTaskFailure = createAction('[GOAL API] Update Task Failure',props<{error: TaskApiResponse}>());
 
 export const GoalApiActions = {
   loadSuccess,
   loadFailure,
+  getGoalByIdSuccess,
+  getGoalByIdFailure,
   createSuccess,
   createFailure,
   updateSuccess,
   updateFailure,
   deleteSuccess,
   deleteFailure,
+  updateTaskSuccess,
+  updateTaskFailure
 }
